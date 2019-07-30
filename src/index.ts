04395063d2a71128a6fc4bcb91101425f0a91a56
Dhaya Benmessaoud <154633+dhayab@users.noreply.github.com>
2019-07-30 15:37:22 +0200

    feat: angular boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-import './index.scss';
+import { ViewEncapsulation } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-console.log('Hello World!');
+import { AppModule } from './app/app.module';
+import { hmrBootstrap } from './hmr';
+
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule, {
+	defaultEncapsulation: (document.head as any).attachShadow ? ViewEncapsulation.ShadowDom : ViewEncapsulation.Emulated,
+	preserveWhitespaces: true,
+});
+
+// @ts-ignore TS2304
+if (module.hot) {
+	// @ts-ignore TS2304
+	hmrBootstrap(module, bootstrap);
+} else {
+	bootstrap();
+}
