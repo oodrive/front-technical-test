8c277b00700777d05041b9f748ef764704bbfcf3
Dhaya Benmessaoud <154633+dhayab@users.noreply.github.com>
2019-08-06 15:48:23 +0200

    fix: disable native encapsulation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { ViewEncapsulation } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { hmrBootstrap } from './hmr';
 
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule, {
-	defaultEncapsulation: (document.head as any).attachShadow ? ViewEncapsulation.ShadowDom : ViewEncapsulation.Emulated,
 	preserveWhitespaces: true,
 });
 
