3a063b855fb1f29e2e5d2a81de281e14b202437c
nbelhadj <nbelhadj@vermeg.com>
2020-05-13 02:52:31 +0200

    remove unecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,7 @@ import { openFolder } from './features/folder-explorer/state/folder-explorer.act
 })
 export class AppComponent implements OnInit {
   title = 'front-technical-test';
-  items: MenuItem[] = [
-    // { label: 'Categories' },
-    // { label: 'Sports' },
-    // { label: 'Football' },
-    // { label: 'Countries' },
-    // { label: 'Spain' },
-    // { label: 'F.C. Barcelona' },
-    // { label: 'Squad' },
-    // {
-    //   label: 'Lionel Messi',
-    //   url: 'https://en.wikipedia.org/wiki/Lionel_Messi',
-    //   icon: 'pi pi-external-link',
-    // },
-  ];
+  items: MenuItem[] = [];
 
   home: MenuItem;
 
