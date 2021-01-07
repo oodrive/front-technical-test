6cf88349cac6f1eef695a563aba2cd011655e4da
Mohamed Ali Ben Slimen <mabs.skydev@gmail.com>
2021-01-07 10:28:07 +0100

    init template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
 	imports: [
@@ -9,6 +12,9 @@ import { AppComponent } from './app.component';
 	],
 	declarations: [
 		AppComponent,
+		HeaderComponent,
+		ContentComponent,
+		MenuComponent,
 	],
 	bootstrap: [AppComponent],
 })
